873976b25eb4c526a06bde3d433794f57d08535c
G184452021 <218445@st.takushoku-u.ac.jp>
2024-05-25 12:06:55 +0900

    更新

--- kazuate.js
+++ kazuate.js
@@ -8,15 +8,18 @@ let kaisu = 0;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-hantei();
-hantei();
-hantei();
+let e = document.querySelector('button#hantei');
+e.addEventListener('click', hantei);
+// hantei();
+// hantei();
+// hantei();
+// hantei();
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
+  // let yoso = 4;
+  let yoso = parseInt(document.querySelector('input[name="kazu"]').value);
   
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
@@ -26,12 +29,12 @@ function hantei() {
   span.textContent = kaisu;
   console.log(`${kaisu}回目の予想: ${yoso}`);
   span = document.querySelector('span#answer');
-  if(kaisu < 3) {
+  if(kaisu < 4) {
     if(kotae === yoso) {
         console.log("正解です．おめでとう!");
         span.textContent = "正解です．おめでとう!";
         kaisu = 3;
-    } else if(kaisu === 2) {
+    } else if(kaisu === 3) {
         console.log(`まちがい．残念でした答えは${kotae}です．`);
         span.textContent = `まちがい．残念でした答えは${kotae}です．`;
     } else {
